e07327491b4787efc38909ad821edf443a9fbdf0
Ethan Reesor <firelizzard@gmail.com>
2015-10-14 23:10:23 -0700

    Using option sets

--- main.go
+++ main.go
@@ -8,7 +8,22 @@ import (
    "errors"
 )
 
-var asDaemon = goopt.
+/* ----- General Options ----- */
+
+var genops = goopt.NewOptionSet()
+
+var bus = genops.
+         DefineOption("dbus", "set the DBus bus").
+            ShortNames('b').
+            Names("bus", "dbus-bus").
+         AsString("session")
+
+
+/* ----- Server Mode Options ----- */
+
+var serops = goopt.NewOptionSet()
+
+var asDaemon = serops.
          DefineOption("daemon", "run as a daemon in the background").
             ShortNames('D').
             Names("daemon", "background").
@@ -19,24 +34,41 @@ var asDaemon = goopt.
 
 var inSrc, outDest, errDest *os.File
 
-func init() {
-   goopt.DefineOption("input", "standard input source").
+func initServerModeOptions() {
+   serops.DefineOption("input", "standard input source").
             ShortNames('i').
             Names("input").
             Process(parseFileOpt(os.Stdin, &inSrc)).
          DefineAlternate("standard input file").
             ShortNames('I').
             Names("input-file").
-            Process(parseOptOpenFile(&inSrc))
+            Required().
+            ProcessAsFile(&inSrc, false, fileErrorHandler)
    
-   goopt.DefineOption("output", "standard output source").
+   serops.DefineOption("output", "standard output source").
             ShortNames('o').
             Names("output").
             Process(parseFileOpt(os.Stdout, &outDest)).
          DefineAlternate("standard output file").
             ShortNames('O').
             Names("output-file").
-            Process(parseOptCreateFile(&outDest))
+            Required().
+            ProcessAsFile(&outDest, true, fileErrorHandler)
+   
+   serops.DefineOption("error", "standard error source").
+            ShortNames('e').
+            Names("error").
+            Process(parseFileOpt(os.Stdout, &errDest)).
+         DefineAlternate("standard error file").
+            ShortNames('E').
+            Names("error-file").
+            Required().
+            ProcessAsFile(&errDest, true, fileErrorHandler)
+}
+
+func fileErrorHandler(input string, err error) error {
+   log.Fatalf("Failed to create '%s': %s", input, err.Error())
+   return nil
 }
 
 func parseFileOpt(std *os.File, ptr **os.File) goopt.Processor {
@@ -49,7 +81,10 @@ func parseFileOpt(std *os.File, ptr **os.File) goopt.Processor {
       case "std":
          *ptr = os.Stdin
       case "null":
-         return parseOptOpenFile(ptr)(o, os.DevNull)
+//         return parseOptOpenFile(ptr)(o, os.DevNull)
+          return func (_ *goopt.Option, _ string) error {
+             *ptr = nil
+          }()
       default:
          return errors.New("'" + input + "' is not a supported input source")
       }
@@ -57,36 +92,54 @@ func parseFileOpt(std *os.File, ptr **os.File) goopt.Processor {
    }
 }
 
-func parseOptOpenFile(ptr **os.File) goopt.Processor {
-   return func (o *goopt.Option, input string) error {
-      if input == "" {
-         return o.MissingArg()
-      }
-      
-      var err error
-      *ptr, err = os.Open(input)
-      if (err != nil) {
-         log.Fatalf("Failed to open '%s': %s", input, err.Error())
-      }
-      return nil
-   }
+/* ----- Client Mode Options ----- */
+
+var cliops = goopt.NewOptionSet()
+
+var list = cliops.
+         DefineOption("list-servers", "list the available servers").
+            ShortNames('l').
+            Names("ls", "list", "list-servers").
+         AsFlag(false)
+
+
+/* ----- Main ----- */
+
+func init() {
+   initServerModeOptions()
 }
 
-func parseOptCreateFile(ptr **os.File) goopt.Processor {
-   return func (o *goopt.Option, input string) error {
-      if input == "" {
-         return o.MissingArg()
-      }
-      
-      var err error
-      *ptr, err = os.Create(input)
-      if (err != nil) {
-         log.Fatalf("Failed to create '%s': %s", input, err.Error())
-      }
-      return nil
-   }
+func usageAndExit(err error) {
+   os.Exit(1)
 }
 
 func main() {
+   for _, arg := os.Args {
+      if arg == "--" {
+         mainServer()
+         return
+      }
+   }
+   mainClient()
 }
 
+func mainServer() {
+   opts := goopt.MergeSets(genops, serops)
+   args, err := opts.Parse(os.Args)
+   if err != nil || len(args) < 1 {
+      usageAndExit(err)
+   }
+   
+   proc, err2 := StartProcess(args[0], "What the fuck is the description?", args[1:])
+   if err2 != nil {
+      usageAndExit(err2)
+   }
+}
+
+func mainClient() {
+   opts := goopt.MergeSets(genops, cliops)
+   args, err := opts.Parse(os.Args)
+   if err != nil || len(args) > 0 {
+      usageAndExit(err)
+   }
+}
